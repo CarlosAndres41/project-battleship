09337010923af5fc5a03133f92191cee5e80dd11
Carlos Andrés <ca.castaneda@outlook.com>
2023-04-10 15:56:20 -0500

    Add shipLength object

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let shipLength = {
+    carrier: 5,
+    battleship: 4,
+    destroyer: 3,
+    submarine: 3,
+    patrol: 2,
+};
+
 function createShip(length) {
     const ship = {
         length: length,
